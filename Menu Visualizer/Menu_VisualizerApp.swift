f48f707f2625d0be60dd4148103a0dc0aadfb440
Bruce Wang <88688323+BruceWangyq@users.noreply.github.com>
2025-08-19 16:13:26 -0400

    Initial Commit

---
+++ Menu Visualizer/Menu_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_VisualizerApp.swift
+//  Menu Visualizer
+//
+//  Created by wang yuqiu on 2025-08-19.
+//
+
+import SwiftUI
+
+@main
+struct Menu_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Menu VisualizerTests/Menu_VisualizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_VisualizerTests.swift
+//  Menu VisualizerTests
+//
+//  Created by wang yuqiu on 2025-08-19.
+//
+
+import Testing
+@testable import Menu_Visualizer
+
+struct Menu_VisualizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
